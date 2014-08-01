ffbeb5831659694fc554a3c0b60c784fe38dc2e3
Mike <mike@bluefroggaming.com>
2014-08-01 01:09:20 -0400

    solved problem 6

---
+++ problem6.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+=begin
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+=end
+
+def sum_of_squares(start, stop)
+  return (start..stop).inject { |s, x| s + x ** 2 }
+end
+
+def square_of_sum(start, stop)
+  return (start..stop).inject { |s, x| s + x } ** 2
+end
+
+puts square_of_sum(1, 100) - sum_of_squares(1, 100)
